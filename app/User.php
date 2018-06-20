beb36e5a32f348a66513ca826f2bbfcd4e60056f
shingo.sawada <ec2-user@ip-172-31-15-163.us-east-2.compute.internal>
2018-06-20 07:48:37 +0000

    Profile

---
+++ app/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    
+
+
+
+    public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
+    
+    public function feed_microposts()
+    {
+        $follow_user_ids = $this->followings()-> pluck('users.id')->toArray();
+        $follow_user_ids[] = $this->id;
+        return Micropost::whereIn('user_id', $follow_user_ids);
+    }
+    
+}
